96c0271ae1de7210a213a211cf97e76eda6b8acf
devHectorGa <contacto@devHector.Ga>
2020-04-05 19:26:39 -0500

    Hash Collisions

---
+++ 02-hash/01.hashCollisions.js
@@ -0,0 +1,18 @@
+let user = {
+  age: 54,
+  name: "Wylie",
+  magic: true,
+  scream: function() {
+    console.log("Ahh");
+  }
+};
+
+console.log(user);
+console.log(user.age); // O(1)
+
+user.spell = "Abra Kadabra"; // O(1)
+
+console.log(user);
+console.log(user.scream());
+
+// O(n/k)
