47bd5ec5849b05d5cc9ea87a1dc79b6a21e7f38e
devHectorGa <contacto@devHector.Ga>
2020-04-06 20:34:04 -0500

    Solution: First Recurring Character

--- 02-hash/03-firstRecurringCharacter.js
+++ 02-hash/03-firstRecurringCharacter.js
@@ -19,6 +19,7 @@ function firstRecurringCharacter(array) {
       keys[array[i]] = true;
     }
   }
+
   return undefined;
 }
 
@@ -26,6 +27,37 @@ const array1 = [2, 5, 1, 2, 3, 5, 1, 2, 4];
 const array2 = [2, 1, 1, 2, 3, 5, 1, 2, 4];
 const array3 = [2, 3, 4, 5];
 
-console.log(firstRecurringCharacter(array1));
-console.log(firstRecurringCharacter(array2));
-console.log(firstRecurringCharacter(array3));
+// SOLUTION
+
+function firstRecurringCharacter2(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+} // O(n^2)
+
+function firstRecurringCharacter3(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]]) {
+      return input[i];
+    } else {
+      map[input[i]] = true;
+    }
+  }
+  return undefined;
+} // O(n)
+
+// console.log(firstRecurringCharacter(array1));
+// console.log(firstRecurringCharacter(array2));
+// console.log(firstRecurringCharacter(array3));
+// console.log(firstRecurringCharacter2(array1));
+// console.log(firstRecurringCharacter2(array2));
+// console.log(firstRecurringCharacter2(array3));
+console.log(firstRecurringCharacter3(array1));
+console.log(firstRecurringCharacter3(array2));
+console.log(firstRecurringCharacter3(array3));
