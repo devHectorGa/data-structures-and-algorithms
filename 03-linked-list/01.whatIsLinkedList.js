350d6189bfb3cc5c71011f03e9f0dbfa775d109b
devHectorGa <contacto@devHector.Ga>
2020-04-08 21:46:33 -0500

    Insert

--- 03-linked-list/01.whatIsLinkedList.js
+++ 03-linked-list/01.whatIsLinkedList.js
@@ -73,12 +73,38 @@ class LinkedList {
     return array;
   }
   insert(index, value) {
-    return this;
+    // check params
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value,
+      next: null
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
   }
+  traverseToIndex(index) {
+    //check params
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {}
 }
 
 const myLinkedList = new LinkedList(10);
-console.log(myLinkedList.append(5));
-console.log(myLinkedList.append(16));
-console.log(myLinkedList.prepend(1));
-console.log(myLinkedList.printList(2, 99));
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+console.log(myLinkedList.printList());
