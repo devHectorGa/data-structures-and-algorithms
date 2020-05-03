0829e9a716d9e5ac20de55cb1cb726d64fc3b8d4
devHectorGa <contacto@devHector.Ga>
2020-05-03 10:36:29 -0500

    Stacks vs Queues

---
+++ 04.stacks/01.stacksVsQueues.js
@@ -0,0 +1,15 @@
+// stacks
+
+// google
+// Udemy
+// YouTube
+
+// Arrays
+// Linked Lists
+
+// Queues
+//   0      1       2       3     <- Array
+// Matt -- Joy -- Samir -- Mack
+// Head                    Tail   <- Linked List
+// Arrays
+// Linked Lists
