a7f5ac0d79be6a8d9de66360213a18f415c3913b
devHectorGa <contacto@devHector.Ga>
2020-07-28 15:42:16 -0500

    Solution of conflict in archives

--- 04.stacks/03.StackImplementation.js
+++ 04.stacks/03.StackImplementation.js
@@ -7,18 +7,36 @@ class Node {
 
 class Stack {
   constructor() {
-    this.pop = null;
+    this.top = null;
     this.bottom = null;
     this.length = 0;
   }
-  peek() {}
-  push(value) {}
-  pop() {}
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    const holdingPoint = this.top;
+    this.top = this.top.next;
+    return holdingPoint;
+  }
   // isEmpty
 }
 
 const myStack = new Stack();
-
+console.log(myStack.peek());
 // Discord
 // Udemy
 // Google
