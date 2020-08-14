a12291ee003a3aaf206f9b0d7c0e7c4938fbacca
devHectorGa <contacto@devHector.Ga>
2020-08-14 16:36:55 -0500

    O(log n)

--- 05.trees/01.binaryTree.js
+++ 05.trees/01.binaryTree.js
@@ -9,5 +9,15 @@ function BinaryTreeNode(value) {
   insert    O(log N)
   delete    O(log N)
 
-    O (log N)
+  O (log N)
+
+  Level 0: 2^0 = 1
+  Level 1: 2^1 = 2
+  Level 2: 2^2 = 4
+  Level 3: 2^3 = 8
+
+  # of nodes = 2^h - 1        h = height
+  log nodes = height
+
+  log 100 = 2
 */
