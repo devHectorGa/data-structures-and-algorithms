56190d7250a018b0e2af1fb9dbce0de94c535d54
devHectorGa <contacto@devHector.Ga>
2020-08-14 16:16:02 -0500

    Binary Tree

---
+++ 05.trees/01.binaryTree.js
@@ -0,0 +1,13 @@
+function BinaryTreeNode(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+/*  BINARY SEARCH TREE
+  lookup    O(log N)
+  insert    O(log N)
+  delete    O(log N)
+
+    O (log N)
+*/
