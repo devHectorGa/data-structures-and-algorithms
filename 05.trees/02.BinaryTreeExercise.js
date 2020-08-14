69474fe21a940f20b9996cb47e6c435273a211d7
devHectorGa <contacto@devHector.Ga>
2020-08-14 18:45:32 -0500

    Solution: insert()

--- 05.trees/02.BinaryTreeExercise.js
+++ 05.trees/02.BinaryTreeExercise.js
@@ -10,7 +10,29 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  insert(value) {}
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
   delete(value) {}
   // remove
 }
@@ -23,7 +45,7 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-JSON.stringify(traverse(tree.root));
+console.log(JSON.stringify(traverse(tree.root)));
 //     9
 //  4     20
 // 1 6  15 170
